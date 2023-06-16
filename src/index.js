be968f3108a072934eec3897c2673a4b938b9730
subhamkumarr <kumarsubham75861@gmail.com>
2023-06-16 11:04:18 +0530

    Added get user by id in repository

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const {PORT} = require('./config/serverConfig');
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+
+// const db = require('./models/index');
 
 const app = express();
 
 const prepareAndStartServer = () => {
 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes);
+
     app.listen(PORT, () => {
-        console.log(`Server started at port ${PORT}`);
+        console.log(`Server Started on Port: ${PORT}`);
+        // if(process.env.DB_SYNC) {
+        //     db.sequelize.sync({alter: true});
+        // }
     });
-   
-}
+}   
 
 prepareAndStartServer();
---
+++ src/routes/v1/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const UserController = require('../../controllers/user-controllers');
+// const {AuthRequestValidators} = require('../../middlewares/index');
+
+const router = express.Router();
+
+router.post('/signup', 
+UserController.create
+);
+// router.post(
+//     '/signin',
+//     AuthRequestValidators.validateUserAuth,
+//     UserController.signIn
+// );
+
+// router.get(
+//     '/isAuthenticated',
+//     UserController.isAuthenticated
+// );
+
+// router.get(
+//     '/isAdmin',
+//     AuthRequestValidators.validateIsAdminRequest,
+//     UserController.isAdmin
+// );
+
+module.exports = router;
